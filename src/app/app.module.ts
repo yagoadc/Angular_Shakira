a87804ce5f4ff28fc6d3e8e22b440359f69b3f6c
Yago Alves <yagoadc@gmail.com>
2018-01-25 14:04:09 -0200

    + Inicio do projeto com angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AlunosComponent } from './alunos/alunos.component';
+
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlunosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
